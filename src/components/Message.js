9227adc3465deeb557202a89bcd5d35ca4cae5ca
Thomas Henderson <tbhenderson55@yahoo.com>
2019-08-28 19:40:42 +0900

    some card styling and dayjs added

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,23 +3,39 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Link from 'react-router-dom/Link'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
-const styles ={ 
+
+const styles = { 
   card: {
     display: 'flex',
+    marginBottom: 20,
+    height: "20%"
+  },
+  image: {
+    minWidth: 100,
+    maxWidth: 150,
+    objectFit: "cover"
+  }, 
+  content: {
+    paddingLeft: 30,
   }
 }
 
-const Message = ({ message, classes, user }) => {
+const Message = ({ message, classes }) => {
+  dayjs.extend(relativeTime)
   return (
-    <Card style={{margin: "20px"}}>
-      <CardContent>
+    <Card className={classes.card}>
+      <CardMedia image="https://source.unsplash.com/random" title="avatar" className={classes.image}></CardMedia>
+      <CardContent className={classes.content}>
         <Typography variant="body1">{message.content}</Typography>
-        <Typography variant="body2" color="textSecondary" >{message.createdAt}</Typography>
-        <Typography variant="h6" component={Link} to={`/profile/${message.user.id}`}>{message.user.username}</Typography>
+        <Typography variant="body2" color="textSecondary" >{dayjs(message.createdAt).fromNow()}</Typography>
+        <Typography variant="h6" color="primary" component={Link} to={`/profile/${message.user.id}`}>{message.user.username}</Typography>
       </CardContent>
     </Card>
   )
