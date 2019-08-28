951754d86cf4b1236b9fa488b68f75a3ae680ab4
Thomas Henderson <tbhenderson55@yahoo.com>
2019-08-28 20:46:50 +0900

    subscription working when create a new message

--- src/components/AllMessages.js
+++ src/components/AllMessages.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { API, graphqlOperation } from "aws-amplify";
 import { listMessages } from "../graphql/queries";
+import { onCreateMessage} from "../graphql/subscriptions";
 import { Connect } from "aws-amplify-react";
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Message from './Message'
-
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Message from "./Message";
 
 export default function AllMessages() {
+  const onNewMessage = (prevQuery, newData) => {
+    let updatedQuery = { ...prevQuery };
+    const updatedMessageList = [
+      newData.onCreateMessage,
+      ...prevQuery.listMessages.items
+    ];
+    updatedQuery.listMessages.items = updatedMessageList;
+    return updatedQuery;
+  };
 
   // useEffect(() => {
   //   getMessages()
@@ -21,34 +30,34 @@ export default function AllMessages() {
   //   }
   // }
 
-
   return (
-    <Connect 
+    <Connect
       query={graphqlOperation(listMessages)}
-      // subscription={graphqlOperation(onCreateMarket)}
-      // onSubscriptionMsg={onNewMarket}
+      subscription={graphqlOperation(onCreateMessage)}
+      onSubscriptionMsg={onNewMessage}
     >
-     {({ data, loading, errors }) => {
-        if (errors.length > 0) return console.log(errors)
-        if (loading || !data.listMessages) return <CircularProgress fullscreen={true} />;
-        console.log(data.listMessages.items)
+      {({ data, loading, errors }) => {
+        if (errors.length > 0) return console.log(errors);
+        if (loading || !data.listMessages)
+          return <CircularProgress fullscreen={true} />;
+        console.log(data.listMessages.items);
         return (
           <>
-            {data.listMessages.items && data.listMessages.items.map(message => (
-              <Message message={message} key={message.id}/>
-            ))}
+            {data.listMessages.items &&
+              data.listMessages.items.map(message => (
+                <Message message={message} key={message.id} />
+              ))}
           </>
         );
       }}
     </Connect>
   );
-              // <div> {
-              // listEvents ? (
-              //     <ListView events={listEvents.items}/>
-              //     ) : (
-              //     <h3> Loading </h3>
-              // )} </div>
+  // <div> {
+  // listEvents ? (
+  //     <ListView events={listEvents.items}/>
+  //     ) : (
+  //     <h3> Loading </h3>
+  // )} </div>
 }
-
 
 // const markets =  data.listMessages.items
